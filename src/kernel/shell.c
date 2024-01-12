692eaa07fa142f14a24e72659b939e428d81c23e
Logan Kissel <kissellogan10@gmail.com>
2024-01-12 15:43:13 -0500

    Changed some colors

--- src/kernel/shell.c
+++ src/kernel/shell.c
@@ -5,6 +5,7 @@
 
 void InitShell()
 {
+    Term_PrintString("\nShell loaded!");
     Term_PrintString("\n");
     sh_newline();
 }
@@ -15,6 +16,9 @@ void ShellHandleCommands()
 
 void sh_newline()
 {
-    uint8_t new_color = get_vga_color(VGA_COLOR_CYAN, VGA_COLOR_BLACK);
-    Term_PrintString("\nOS>");
+    uint8_t new_color = get_vga_color(VGA_COLOR_LIGHTCYAN, VGA_COLOR_BLACK);
+    Term_SetColor(new_color);
+    Term_PrintString("\nOS> ");
+    uint8_t old_color = get_vga_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+    Term_SetColor(old_color);
 }
