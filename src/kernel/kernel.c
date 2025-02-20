442d9fc496aab714ea79a8b61aa66afb8bdc0315
Logan Kissel <kissellogan10@gmail.com>
2025-02-20 10:24:31 -0500

    Fixed warning message for misnamed attribute

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,13 +6,13 @@
 // Set base revision to 2, recommended version
 __attribute__((used, section(".requests"))) static volatile LIMINE_BASE_REVISION(2);
 
-__attribute__((use, section(".requests"))) static volatile struct limine_framebuffer_request framebuffer_request = {
+__attribute__((used, section(".requests"))) static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0};
 
 // Start and end markers for Limine requests, can be moved to any C file
 __attribute__((used, section(".requests_start_marker"))) static volatile LIMINE_REQUESTS_START_MARKER;
-__attribute__((use, section(".requests_end_marker"))) static volatile LIMINE_REQUESTS_END_MARKER;
+__attribute__((used, section(".requests_end_marker"))) static volatile LIMINE_REQUESTS_END_MARKER;
 
 static void halt(void)
 {
@@ -49,19 +49,19 @@ void putc(struct limine_framebuffer *fb, char c)
 // Kernel entry point
 void kmain(void)
 {
-    // Ensure the bootloader actually understands our base revision (see spec).
+    // Ensure the bootloader actually understands our base revision (see spec)
     if (LIMINE_BASE_REVISION_SUPPORTED == false)
     {
         halt();
     }
 
-    // Ensure we got a framebuffer.
+    // Ensure we got a framebuffer
     if (framebuffer_request.response == NULL || framebuffer_request.response->framebuffer_count < 1)
     {
         halt();
     }
 
-    // Fetch the first framebuffer.
+    // Fetch the first framebuffer
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
