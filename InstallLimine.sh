316de44df5b34651208f2afe8e3d722d19197a44
Logan Kissel <kissellogan10@gmail.com>
2026-07-06 14:10:31 -0400

    Update Limine to revision 6

--- InstallLimine.sh
+++ InstallLimine.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 # Download the latest Limine binary release for the 7.x branch.
-git clone https://github.com/limine-bootloader/limine.git --branch=v7.x-binary --depth=1
+git clone https://github.com/limine-bootloader/limine.git --branch=v11.x-binary --depth=1
 
 # Build "limine" utility.
 make -C limine
